b9d61ae9ea5ea701777337cd13ae30c9de0726ae
Gulzhan <114820316+GulzhanKarakul@users.noreply.github.com>
2026-03-19 23:13:17 +0500

    add project foundation
    - add config package
    - add database package
    - add graceful shutdowm to main
    - add Dockerfile
    - add docker-compose

---
+++ cmd/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/GulzhanKarakul/payment-service/pkg/config"
+	"github.com/GulzhanKarakul/payment-service/pkg/database"
+)
+
+func main() {
+	// Srtuctured logger
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	logger.Info("starting payment-service...")
+	cfg := config.Load()
+
+	// connect to database
+	db, err := database.NewPostgres(database.DefaultConfig(cfg.DSN()))
+	if err != nil{
+		logger.Error("failed to connect to database", "error", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+	logger.Info("connected to database")
+
+	// Http server with timeout
+	srv := &http.Server{
+		Addr: ":" + cfg.Server.Port,
+		Handler: nil,
+		ReadTimeout: 15 * time.Second, // request timeout
+		WriteTimeout: 15 * time.Second, // response timeout
+		IdleTimeout: 60 * time.Second, // keep-alive timeout
+	}
+	logger.Info("server started", "port", cfg.Server.Port)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Error("server failed", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	// Graceful shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	logger.Info("server shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		logger.Error("forced shutdown", "error", err)
+	}
+
+	logger.Info("server stopped")
+}
---
+++ pkg/config/config.go
@@ -0,0 +1,75 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+// Config holds all application configurations
+type Config struct {
+	Server ServerConfig
+	Database DatabaseConfig
+	Redis RedisConfig
+	Kafka KafkaConfig
+}
+
+// ServerConfig holds HTTP server settings
+type ServerConfig struct {
+	Port string
+}
+
+// DatabaseConfig holds PostgreSQL settings
+type DatabaseConfig struct {
+	Host string
+	Port string
+	User string
+	Password string
+	Name string
+	SSLMode string
+}
+
+// RedisConfig holds Redis settings
+type RedisConfig struct {
+	Host string
+	Port string
+}
+
+// KafkaConfig holds Kafka settings
+type KafkaConfig struct {
+	Brokers string
+}
+
+// Load func reads configurations from environment variables
+// it attempts to load .env file but doesnt fail if not found
+func Load() *Config{
+ godotenv.Load()
+
+	return &Config{
+		Server:  ServerConfig{
+			Port:  os.Getenv("SERVER_PORT"),
+		},
+		Database:  DatabaseConfig{
+			Host:  os.Getenv("DB_HOST"),
+			Port:  os.Getenv("DB_PORT"),
+			User:  os.Getenv("DB_USER"),
+			Password:  os.Getenv("DB_PASSWORD"),
+			Name: os.Getenv("DB_NAME"),
+			SSLMode:  os.Getenv("DB_SSLMODE"),
+		},
+		Redis: RedisConfig{
+			Host: os.Getenv("REDIS_HOST"),
+			Port: os.Getenv("REDIS_PORT"),
+		},
+		Kafka: KafkaConfig{
+			Brokers: os.Getenv("KAFKA_BROKERS"),
+		},
+	}
+}
+
+// DSN returns PostgreSQL connection string
+func (c *Config) DSN() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+	c.Database.Host, c.Database.Port, c.Database.User, c.Database.Password, c.Database.Name, c.Database.SSLMode)
+}
